352a9500c5d0f58a6428aff1f9313d5763bda691
Aleksandros Sobczyk <6952514+asobczyk@users.noreply.github.com>
2026-02-16 10:54:38 +0000

    Fix test for inv trick for any dim

--- tests/test_tri_inv_trick.py
+++ tests/test_tri_inv_trick.py
@@ -32,12 +32,12 @@ def block_ones_matrix(n, block_dim_x, block_dim_y):
     U_ = np.ones((16, 16))
     n_blocks = n // 16
     U = np.zeros((block_dim_x, block_dim_y, n, n))
-    for k in range(block_dim_x):
-        for l in range(block_dim_y):
+    for x in range(block_dim_x):
+        for y in range(block_dim_y):
             for i in range(n_blocks):
                 start = i * 16
                 end = i * 16 + 16
-                U[k, l, start:end, start:end] = U_
+                U[x, y, start:end, start:end] = U_
     return torch.from_numpy(np.triu(U, 1))
 
 
@@ -45,10 +45,10 @@ def block_random_matrix(n, block_dim_x, block_dim_y, scale=0.2):
     U_ = scale * np.random.rand(16, 16)
     U_ = np.triu(U_, k=1)
     U = np.zeros((block_dim_x, block_dim_y, n, n))
-    for k in range(block_dim_x):
-        for l in range(block_dim_y):
+    for x in range(block_dim_x):
+        for y in range(block_dim_y):
             for i in range(0, n, 16):
-                U[k, l, i : i + 16, i : i + 16] = U_.copy()
+                U[x, y, i : i + 16, i : i + 16] = U_.copy()
     return torch.from_numpy(U)
 
 
@@ -63,10 +63,10 @@ def _test_tri_inv_trick(U: torch.tensor, atol: float, rtol: float, ftol: float):
     Identity = np.triu(Identity)
     Identity = np.tril(Identity)
     golden_numpy = np.zeros((U.shape))
-    for k in range(U.shape[0]):
-        for l in range(U.shape[1]):
-            golden_numpy[k, l] = np.linalg.inv(
-                U[k, l].numpy().astype(np.double) + Identity
+    for x in range(U.shape[0]):
+        for y in range(U.shape[1]):
+            golden_numpy[x, y] = np.linalg.inv(
+                U[x, y].numpy().astype(np.double) + Identity
             )
     golden_cpu = torch.from_numpy(golden_numpy)
 
@@ -82,8 +82,6 @@ def _test_tri_inv_trick(U: torch.tensor, atol: float, rtol: float, ftol: float):
     )
     actual_numpy = actual_cpu.numpy()
     golden_numpy = golden_cpu.numpy()
-    # print(golden_cpu.shape)
-    # print(actual_cpu.shape)
 
     assert np.allclose(
         actual_numpy, golden_numpy, atol=atol, rtol=rtol
