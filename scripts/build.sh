ae8450e2a0d7947191866fda6c97e3b618b364f2
Anastasios Zouzias <zouzias@gmail.com>
2026-02-17 14:16:32 +0000

    fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -43,7 +43,7 @@ source "$_ASCEND_INSTALL_PATH"/bin/setenv.bash
 echo "Current compile soc version is ${SOC_VERSION}"
 
 # See https://docs.pytorch.org/cppdocs/installing.html
-CMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}:$(python3 -c 'import torch; print(torch.utils.cmake_prefix_path, end="")')
+CMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}:$(python -c 'import torch; print(torch.utils.cmake_prefix_path)')
 export CMAKE_PREFIX_PATH
 
 
@@ -56,6 +56,7 @@ mkdir -p build
 cmake -S "${PARENT_DIR}" \
       -B build \
       -DSOC_VERSION="${SOC_VERSION}" \
+      -DCMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH} \
       -DASCEND_CANN_PACKAGE_PATH="${_ASCEND_INSTALL_PATH}"
 
 cmake --build build  -j
