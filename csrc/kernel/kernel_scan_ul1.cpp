8a5c1225d6b3c79ef0090d0bba8d175aace29bc5
Giacomo Castiglioni <giacomo.castiglioni@huawei.com>
2026-04-17 13:32:13 +0000

    compiling skeleton

--- csrc/kernel/kernel_scan_ul1.cpp
+++ csrc/kernel/kernel_scan_ul1.cpp
@@ -6,28 +6,9 @@ See LICENSE in the root of the software repository:
 https://github.com/huawei-csl/pto-kernels/
 for the full License text.
 */
-#if defined __CCE_AICORE__ == 220 && defined(__DAV_C220_VEC__)
-
-// Placeholder for VEC compilation (the real kernel is CUBE-only).
-#define MEMORY_BASE
-#include <pto/common/type.hpp>
-
-extern "C" __global__ AICORE void scan_ul1_fp16(__gm__ void* x,
-                                                     __gm__ void* s,
-                                                     uint32_t matrix_size) {}
-
-extern "C" __global__ AICORE void scan_ul1_fp32(__gm__ void* x,
-                                                     __gm__ void* s,
-                                                     uint32_t matrix_size) {}
-
-#elif (__CHECK_FEATURE_AT_PRECOMPILE) || \
-    (__CCE_AICORE__ == 220 && defined(__DAV_C220_CUBE__))
-
-#define MEMORY_BASE
-
-#include <pto/pto-inst.hpp>
 
 #define GM_ADDR __gm__ uint8_t*  // To avoid #include "kernel_operator.h"
+#include <pto/pto-inst.hpp>
 
 using namespace pto;
 
@@ -35,35 +16,40 @@ constexpr unsigned UB_SIZE = 0x30000;  // 192KB UB of A2A3
 
 
 template <typename InputT, typename OutputT, uint32_t matrix_size>
-AICORE void runKernelSimpleMatMul(__gm__ InputT* a,
-                                  __gm__ OutputT* c) {
+AICORE void runKernelScanUl1(__gm__ InputT* x,
+                                  __gm__ OutputT* s) {
 
+#if (__CHECK_FEATURE_AT_PRECOMPILE) || \
+    (__CCE_AICORE__ == 220 && defined(__DAV_C220_CUBE__))
+#else
+// Nothing to do on VEC
+#endif
 }
 
 template <typename T>
-AICORE void run_scan_ul1(__gm__ T* a, __gm__ float* c,
+AICORE void run_scan_ul1(__gm__ T* x, __gm__ float* s,
                               uint32_t matrix_size) {
   static_assert(std::is_same_v<T, half> or std::is_same_v<T, float>,
                 "scan_ul1 supports only fp16/fp32.");
 
   switch (matrix_size) {
     case 16:
-      runKernelSimpleMatMul<T, float, 16>(a, b, c);
+      runKernelScanUl1<T, float, 16>(x, s);
       break;
     case 32:
-      runKernelSimpleMatMul<T, float, 32>(a, b, c);
+      runKernelScanUl1<T, float, 32>(x, s);
       break;
 
     case 64:
-      runKernelSimpleMatMul<T, float, 64>(a, b, c);
+      runKernelScanUl1<T, float, 64>(x, s);
       break;
 
     case 96:
-      runKernelSimpleMatMul<T, float, 96>(a, b, c);
+      runKernelScanUl1<T, float, 96>(x, s);
       break;
 
     case 128:
-      runKernelSimpleMatMul<T, float, 128>(a, b, c);
+      runKernelScanUl1<T, float, 128>(x, s);
       break;
   }
 }
@@ -71,15 +57,13 @@ AICORE void run_scan_ul1(__gm__ T* a, __gm__ float* c,
 extern "C" __global__ AICORE void scan_ul1_fp16(__gm__ void* x,
                                                      __gm__ void* s,
                                                      uint32_t matrix_size) {
-  run_scan_ul1<half>((__gm__ half*)a, (__gm__ float*)c,
+  run_scan_ul1<half>((__gm__ half*)x, (__gm__ float*)s,
                           matrix_size);
 }
 
 extern "C" __global__ AICORE void scan_ul1_fp32(__gm__ void* x,
                                                      __gm__ void* s,
                                                      uint32_t matrix_size) {
-  run_scan_ul1<float>((__gm__ float*)a, (__gm__ float*)c,
+  run_scan_ul1<float>((__gm__ float*)x, (__gm__ float*)s,
                            matrix_size);
 }
-
-#endif
