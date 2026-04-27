7d874d7986cd278ec646b207edf4efa5d33a7986
anastasios <anastasios.zouzias@huawei.com>
2026-04-27 15:31:31 +0000

    add gelems per sec

--- benchmarking/bench_tri_inv_rec_unroll.py
+++ benchmarking/bench_tri_inv_rec_unroll.py
@@ -69,10 +69,11 @@ def bench(path: str = "benchmark_data/tri_inv_rec_unroll.csv"):
                 n_el = U.numel()
                 # I/O: Input and output have 16 bits, so (2 + 2) bytes per element
                 gbps = (2 + 2) * n_el / (ms / 1e3) / 1e9
+                gelems = n_el / (ms / 1e3) / 1e9
                 dtype_name = "fp16" if dtype == torch.float16 else "bf16"
                 print(
                     f"{dtype_name}, N={n}, bdx={block_dim_x}, bdy={block_dim_y}, "
-                    f"{ms:.3f} ms, {gbps:.3f} GB/s"
+                    f"{ms:.3f} ms, {gbps:.3f} GB/s, {gelems:.3f} Gelems/s"
                 )
                 rows.append([dtype_name, n, block_dim_x, block_dim_y, ms, gbps])
 
