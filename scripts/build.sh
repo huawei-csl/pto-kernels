7774bdf70e5c5051673c1e9c06121a42b901191a
anastasios <anastasios.zouzias@huawei.com>
2026-05-15 07:00:15 +0000

    update scripts

--- scripts/build.sh
+++ scripts/build.sh
@@ -32,10 +32,10 @@ if [ -n "$ASCEND_INSTALL_PATH" ]; then
 elif [ -n "$ASCEND_HOME_PATH" ]; then
     _ASCEND_INSTALL_PATH="$ASCEND_HOME_PATH"
 else
-    if [ -d "$HOME/Ascend/ascend-toolkit/latest" ]; then
-        _ASCEND_INSTALL_PATH="$HOME"/Ascend/ascend-toolkit/latest
+    if [ -d "$HOME/Ascend/cann/latest" ]; then
+        _ASCEND_INSTALL_PATH="$HOME"/Ascend/cann/latest
     else
-        _ASCEND_INSTALL_PATH=/usr/local/Ascend/ascend-toolkit/latest
+        _ASCEND_INSTALL_PATH=/usr/local/Ascend/cann/latest
     fi
 fi
 # shellcheck source=/dev/null
